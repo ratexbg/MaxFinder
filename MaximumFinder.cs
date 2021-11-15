d1463f67b8d40e533b064b3d66e2392992a70460
ratexbg <83062383+ratexbg@users.noreply.github.com>
2021-11-15 18:22:25 +0200

    Initial commit

---
+++ MaximumFinder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxFinder
+{
+    class MaximumFinder
+    {
+        public void DetermineMaximum()
+        {
+            Console.WriteLine("Enter three floating-point values,\n"
+                +"pressing 'Enter' after each one: ");
+
+
+            double number1 = Convert.ToDouble(Console.ReadLine());
+            double number2 = Convert.ToDouble(Console.ReadLine());
+            double number3 = Convert.ToDouble(Console.ReadLine());
+
+
+            double result = Maximum(number1,number2,number3);
+
+            Console.WriteLine("Maximum is : "+ result);
+        }
+
+        public double Maximum(double x, double y, double z) 
+        {
+            double maximumValue = x;
+
+            if (y > maximumValue)
+            {
+                maximumValue = y;
+            }
+            if (z> maximumValue)
+            {
+                maximumValue = z;
+            }
+            return maximumValue;
+        }
+    }
+}
